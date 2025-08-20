35737ea4327d8a73f965bd95a014a6b95bd77328
Jattin Manhas <jattin.stpl@gmail.com>
2025-08-20 17:29:39 +0530

    Solved Logic Building Questions

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import RevisitArrays.Fundamentals.ArraysFundamentals;
+import RevisitArrays.LogicBuilding.ArraysLogic;
+
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -24,5 +27,23 @@ public class Main {
 
         int[] leftRotate = new int[]{3, 4, 1, 5, 3, -5};
         af.rotateArray(leftRotate, 8);
+
+//        ============================ ARRAYS LOGIC BUILDING ====================
+        ArraysLogic al = new ArraysLogic();
+        int[] moveZeros = new int[]{0, 20, 0, -20, 0, 20};
+        al.moveZeroes(moveZeros);
+
+        int[] removeDuplicates = new int[]{0, 0, 3, 3, 5, 6};
+        System.out.println(al.removeDuplicates(removeDuplicates));
+
+        int[] missing = new int[]{0, 2, 3, 1, 4};
+        System.out.println(al.missingNumber(missing));
+
+        int[] unionArr1 = new int[]{1,1,2,4,4,6,6,6,7,11,11,12,15,15,16,17,18,21,23,24,24,24,26,30,31,32,34,36,39,42,44,44,45,46,46,48,50};
+        int[] unionArr2 = new int[]{1,4,4,5,7,7,8,9,9,9,12,13,15,16,21,21,25,27,27,28,30,30,32,33,33,34,35,38,38,38,39,39,39,40,41,42,42,42,43,43,44,45,46,46,47,47,47,49,50};
+
+        System.out.println(Arrays.toString(al.unionArray(unionArr1, unionArr2)));
+        System.out.println(Arrays.toString(al.intersectionArray(unionArr1, unionArr2)));
     }
+
 }
--- src/RevisitArrays/LogicBuilding/ArraysLogic.java
+++ src/RevisitArrays/LogicBuilding/ArraysLogic.java
@@ -1,7 +1,137 @@
 package RevisitArrays.LogicBuilding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ArraysLogic {
     public void moveZeroes(int[] nums) {
+        int insertPosition = 0;
 
+        for(int num: nums){
+            if(num != 0){
+                nums[insertPosition++] = num;
+            }
+        }
+
+        while(insertPosition != nums.length){
+            nums[insertPosition++] = 0;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public int removeDuplicates(int[] nums) {
+        int left = 0;
+
+        for(int right = 1; right < nums.length; right++){
+            if(nums[right] != nums[left]){
+                left++;
+                nums[left] = nums[right];
+            }
+        }
+
+        return left + 1;
+    }
+
+    // best approach is the mathematical formula. sum of n number is n*(n+1)/2
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1)/2;
+        int actualSum = 0;
+
+        for(int num: nums){
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    /*
+    * If both arrays are sorted then this merge one is the best solution with
+    * TC = (O(n+m)) AND SC = O(n+m) but this approach can't handle unsorted
+    * output
+    *
+    * If both arrays are not sorted then TreeSet approach is the best as it
+    * sorts all the elements inserted into the set. but it takes log(n) extra time
+    * so TC = O((n+m)log(n+m)) and SC = O(n+m)
+     */
+    public int[] unionArray(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        Integer last = null;
+
+        while(i < nums1.length && j < nums2.length){
+            int val;
+            if(nums1[i] == nums2[j]){
+                val = nums1[i];
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]){
+                val = nums1[i];
+                i++;
+            }
+            else{
+                val = nums2[j];
+                j++;
+            }
+
+            if(last == null || val != last){
+                list.add(val);
+                last = val;
+            }
+        }
+
+        while(i != nums1.length){
+            if(last == null || nums1[i] != last){
+                list.add(nums1[i]);
+                last = nums1[i];
+            }
+            i++;
+        }
+
+        while(j != nums2.length){
+            if(last == null || nums2[j] != last){
+                list.add(nums2[j]);
+                last = nums2[j];
+            }
+            j++;
+        }
+
+        int[] ans = new int[list.size()];
+        for(int l = 0; l < ans.length; l++){
+            ans[l] = list.get(l);
+        }
+
+        return ans;
+    }
+
+    public int[] intersectionArray(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+
+        while(i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+
+        int[] ans = new int[list.size()];
+        for(int l = 0; l < ans.length; l++){
+            ans[l] = list.get(l);
+        }
+
+        return ans;
     }
 }
